c30ff957950ae799dcec0261147f0e3bc278036e
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-02-25 14:15:56 +0100

    Update Sensore_Ultrasuoni.ino

--- Sensore_Ultrasuoni/Sensore_Ultrasuoni.ino
+++ Sensore_Ultrasuoni/Sensore_Ultrasuoni.ino
@@ -1,16 +1,36 @@
 #include <HCSR04.h>
 
+const int elements_number = 10;
+int store_values[elements_number];
+
 int echoPin = 5;
 int trigPin = 4;
 UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("time, distance");
 
 }
 
 void loop() {
- Serial.println(distanceSensor.measureDistanceCm());
- delay(10);
-
+  for (int i = 0; i<elements_number; i ++){
+    int distanza = distanceSensor.measureDistanceCm();
+    if (distanza == -1){
+      Serial.println("OUT OF SCALE");
+    }
+    else{
+      store_values[i] = distanza;
+    }
+    delay(20);
+  }
+ unsigned long media = 0;
+ for (int i = 0; i< elements_number; i++){
+  media += store_values[i];
+ }
+media = media / elements_number;
+Serial.print(millis() / 1000);
+Serial.print(", ");
+Serial.println(media);
+delay(10);
 }
